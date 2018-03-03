baf3ed064fc1abb478ec47fc700982acaf96de49
Jacob Ilin <me@jacobilin.com>
2018-03-03 11:09:35 -0800

    Android app

---
+++ android/app/src/main/java/com/parkaroundtheblock/parkaroundtheblock/BlockParkActivity.java
@@ -0,0 +1,13 @@
+package com.parkaroundtheblock.parkaroundtheblock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlockParkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_block_park);
+    }
+}
